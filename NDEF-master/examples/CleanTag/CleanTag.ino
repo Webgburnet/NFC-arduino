ef483b5d5437393700851e8359b73f9249442b5a
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2019-12-04 08:50:20 +0100

    Add files via upload

---
+++ NDEF-master/examples/CleanTag/CleanTag.ino
@@ -0,0 +1,54 @@
+// Clean resets a tag back to factory-like state
+// For Mifare Classic, tag is zero'd and reformatted as Mifare Classic
+// For Mifare Ultralight, tags is zero'd and left empty
+/*Pour arduino uno spi
+ * SCK : 13
+ * MISO : 12
+ * MOSI : 11
+ * SSEL : 10
+ */
+#if 1
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 10);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+/*Pour arduino uno i2c
+ * SDA : A4
+ * SCL : A5/SCL
+ */ 
+#else
+
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+#endif
+
+void setup(void) {
+    Serial.begin(9600);
+    Serial.println("NFC Tag Cleaner");
+    nfc.begin();
+}
+
+void loop(void) {
+
+    Serial.println("\nPlace a tag on the NFC reader to clean.");
+
+    if (nfc.tagPresent()) {
+
+        bool success = nfc.clean();
+        if (success) {
+            Serial.println("\nSuccess, tag restored to factory state.");
+        } else {
+            Serial.println("\nError, unable to clean tag.");
+        }
+
+    }
+    delay(5000);
+}
---
+++ NDEF-master/examples/WriteTag/WriteTag.ino
@@ -0,0 +1,52 @@
+/*Pour arduino uno spi
+ * SCK : 13
+ * MISO : 12
+ * MOSI : 11
+ * SSEL : 10
+ */
+#if 1
+#include <SPI.h>
+#include <PN532_SPI.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SPI pn532spi(SPI, 10);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+/*Pour arduino uno i2c
+ * SDA : A4
+ * SCL : A5/SCL
+ */       
+#else
+
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+#endif
+
+void setup() {
+      Serial.begin(9600);
+      Serial.println("NDEF Writer");
+      nfc.begin();
+}
+
+void loop() {
+    Serial.println("\nPlace a formatted Mifare Classic or Ultralight NFC tag on the reader.");
+    if (nfc.tagPresent()) {
+        NdefMessage message = NdefMessage();
+        message.addTextRecord("TSF : NFC"); // Text Message you want to Record
+        message.addUriRecord("http://lyc-hbrisson-vierzon.tice.ac-orleans-tours.fr/"); // Website you want to Record
+        message.addTextRecord("BTS SNEC Premiere Année, Année 2016-2017, LPO Henri BRISSON");  // Ednding Message for you to Record
+
+        bool success = nfc.write(message);
+        if (success) {
+          Serial.println("Success. Try reading this tag with your phone.");        
+        } else {
+          Serial.println("Write failed.");
+        }
+    }
+    delay(5000);
+}
